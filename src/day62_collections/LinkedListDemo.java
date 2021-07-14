2b96746fe6d8f099c023b9501a35181831b68fe8
Aziz Niada <niadaaaziz@yahoo.fr>
2021-07-13 20:57:04 -0400

    LinkedList demo (iterate through nodes)

---
+++ src/day62_collections/LinkedListDemo.java
@@ -0,0 +1,18 @@
+package day62_collections;
+import java.util.*;
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        List<Integer> scores = new LinkedList<>();
+        scores.add(5);
+        scores.add(42);
+        scores.add(33);
+        scores.add(33);
+
+        System.out.println(scores);
+        System.out.println(scores.get(0));
+        System.out.println(scores.get(1));
+    }
+}
